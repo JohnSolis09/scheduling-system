86bfb4329ed35282b7b119a8afad2138df523611
johnsolis09 <jlsolis@paterostechnologicalcollege.edu.ph>
2024-05-22 08:00:37 +0800

    commit employee

--- views/employee/_form.php
+++ views/employee/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
 
 /** @var yii\web\View $this */
 /** @var app\models\Employee $model */
@@ -18,7 +19,14 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'no_of_pax')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?= $form->field($model, 'Date_Time')->widget(
+            DatePicker::class,[
+                'options' => ['placeholder' => 'Enter date..'],
+                'pluginOptions' => [
+                    'autoclose' => true,
+                    'format' => 'yyyy/mm/dd',
+                ]
+            ])->label('Date')?>
 
     <?= $form->field($model, 'upadated_at')->textInput() ?>
 
